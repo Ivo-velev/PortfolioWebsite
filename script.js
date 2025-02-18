3208610075a4bb0081e6f1a03785d04483b20243
ivo-velev <ivoemilovvelev71@gmail.com>
2025-02-18 13:28:56 +0000

    uploading my portfolio website

---
+++ script.js
@@ -0,0 +1,32 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+const button1 = document.querySelector('.btn-group .btn1');
+const button2 = document.querySelector('.btn-group .btn2');
+const button3 = document.querySelector('.btn-group .btn3');
+const button4 = document.querySelector('.header .btn4');
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active');
+}
+
+button1.onclick = () => {
+    window.location.href = "https://github.com/Ivo-velev/Livechat"
+}
+button2.onclick = () => {
+    window.location.href = "https://github.com/Ivo-velev/Email"
+}
+button3.onclick = () => {
+    window.location.href = "https://github.com/Ivo-velev/Wiki"
+}
+button4.onclick = () => {
+    window.location.href = "https://github.com/Ivo-velev"
+}
+function scrollToContact() {
+    const contactSection = document.getElementById('contact');
+
+    if (contactSection) { 
+      contactSection.scrollIntoView({
+        behavior: 'smooth' 
+      });
+    }
+  }
